452bc0e84035aecd6e5ac4b75374b9ef1d65c7c3
Brad Knowles <code@bradknowles.com>
2017-11-26 14:26:56 -0500

    Add NewMember test
    This first test serves both as a valid test and validation that the
    project is setup correctly for future tests.

---
+++ Tests.Integration/MemberControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Heuristics.TechEval.Web.Controllers;
+using Heuristics.TechEval.Web.Models;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Tests.Integration
+{
+    [TestFixture]
+    [Author("Brad Knowles")]
+    public class MemberControllerTests
+    {
+        [Test]
+        [Category("Long Running")]
+        [Category("Integration")]
+        public void NewMember_WhenAdding_ShouldSucceed()
+        {
+            // Arrange
+            var sut = new MembersController();
+            var newMember = new NewMember
+            {
+                Name = "John Smith",
+                Email = "thedoctor@gallifrey.net"
+            };
+
+            // Act
+            var result = sut.New(newMember);
+
+            // Assert
+            var context = new TestDataContext();
+            Assert.AreEqual(true, context.Members.Count() == 1);
+        }
+    }
+}
