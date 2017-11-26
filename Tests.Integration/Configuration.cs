e76e049bf354869701175b469527d5f12b2b6c6b
Brad Knowles <code@bradknowles.com>
2017-11-26 13:38:06 -0500

    Create new Tests.Integration project
    Created a new project to hold integration tests using NUnit.
    Configured EntityFramework to create a separate database for testing,
    while using migrations from the Core project.

---
+++ Tests.Integration/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace Tests.Integration
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<TestDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = "..\\Core\\Migrations";
+        }
+    }
+}
---
+++ Tests.Integration/TestDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Heuristics.TechEval.Core.Models;
+
+namespace Tests.Integration
+{
+    internal class TestDataContext : DbContext
+    {
+        public TestDataContext() : base("Database")
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<TestDataContext>());
+        }
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
